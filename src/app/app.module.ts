2e0a49eaaf9cec0156c24e8511aa8e6c09014419
Anders Langseth <anders.langseth@accenture.com>
2017-04-06 22:32:12 +0200

    step 1 completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { IonicStorageModule } from '@ionic/storage';
+
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
+import { AddItemPage } from '../pages/add-item/add-item';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Data } from '../providers/data';
 
 @NgModule({
   declarations: [
@@ -17,11 +21,13 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
+    AddItemPage,
     TabsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -29,12 +35,14 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
+    AddItemPage,
     TabsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    Data,
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/pages/add-item/add-item.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { NavController, ViewController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-add-item',
+  templateUrl: 'add-item.html'
+})
+export class AddItemPage {
+
+	title;
+	description;
+
+	constructor(public nav: NavController, public view: ViewController) {
+
+	}
+
+	saveItem(){
+
+		let newItem = {
+		  title: this.title,
+		  description: this.description,
+      votes: 0
+		};
+
+		this.view.dismiss(newItem);
+
+	}
+
+	close(){
+		this.view.dismiss();
+	}
+
+}
